2cac790c0813c51e0303c0710b11f74e8ba79fe2
joaquinurp <joaquinurp@gmail.com>
2016-03-15 11:39:57 +0100

    cambios en master

--- Lib.java
+++ Lib.java
@@ -6,7 +6,7 @@
 public class Lib {
 
     public void metodoAlumno1(){
-        System.out.println("metodo 1");
+        metodoAlumno2();
     }
 
     public void metodoAlumno2(){
@@ -19,6 +19,6 @@ public class Lib {
 	}
 
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("cambio en master");
     }
 }
